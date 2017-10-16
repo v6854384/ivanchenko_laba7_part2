bbad8e8b748955d7198a96a817fc3de73ab229a2
v6854384 <32385043+v6854384@users.noreply.github.com>
2017-10-16 14:30:55 +0300

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+#include<string>
+using namespace std;
+int main (int argc, char* argv[])
+{
+   string text = argv[1];
+   char symbhol = argv[2][0];
+   for (int i=0; i< text.length(); i++)
+      {
+         if(text[i]==symbhol)
+         cout << i << ebdl;
+      }
+}
